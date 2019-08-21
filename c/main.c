11a772a429d8f9106f0d91ca177b50ddb0dc9383
Nick Silvestri <nickpsilvestri@gmail.com>
2019-08-20 20:46:33 -0400

    add comments and frees in main.c

--- c/main.c
+++ c/main.c
@@ -18,6 +18,7 @@ int main(int argc, char** argv) {
     int image_height = 200;
     int num_samples = 30;
 
+    // write PPM header
     printf("P3\n%d %d\n255\n", image_width, image_height);
 
     // X-axis is left, Y is up, -Z is towards the screen
@@ -26,57 +27,79 @@ int main(int argc, char** argv) {
     vec3* vertical = vec3_make(0.0, 2.0, 0.0);
     vec3* origin = vec3_make(0.0, 0.0, 0.0);
 
+    // manually add surfaces (for now)
     surface* sphere = surface_sphere_make(vec3_make(0, 0, -1), 0.5);
     surface* sphere_big = surface_sphere_make(vec3_make(0, -100.5, -1), 100);
     surface* surfaces[2] = { sphere, sphere_big };
 
+    // iterate over each pixel, from top to bottom (because of PPM pixel order)
     for (int j = image_height - 1; j >= 0; j--) {
+        // and left to right
         for (int i = 0; i < image_width; i++) {
-            vec3* color = vec3_make(0, 0, 0);
+            // create a vec3 to store the total color of this pixel
+            vec3* pixel_color = vec3_make(0, 0, 0);
+
             // sample this pixel num_sample times
             for (int s = 0; s < num_samples; s++) {
                 // u and v are percentages of the width and height of the image
+                // randomness chooses a random point within the square of the pixel
                 float u = ((float) i / image_width) + (drand48() / image_width); 
                 float v = ((float) j / image_height) + (drand48() / image_height);
                 
-                vec3* horizontal_offset = vec3_make(0, 0, 0);
-                horizontal_offset = vec3_scale(horizontal_offset, horizontal, u);
+                // vec3 component for how for how far right to push our final ray
+                vec3* horizontal_offset = vec3_scale(vec3_make(0, 0, 0), horizontal, u);
 
-                vec3* vertical_offset = vec3_make(0, 0, 0);
-                vertical_offset = vec3_scale(vertical_offset, vertical, v);
+                // vec3 component for how far up to push our final ray
+                vec3* vertical_offset = vec3_scale(vec3_make(0, 0, 0), vertical, v);
 
-                vec3* total_offset = vec3_make(0, 0, 0);
-                total_offset = vec3_add(total_offset, horizontal_offset, vertical_offset);
+                // combine the two components above
+                vec3* total_offset = vec3_add(vec3_make(0, 0, 0), horizontal_offset, vertical_offset);
 
-                vec3* direction = vec3_make(0, 0, 0);
-                direction = vec3_add(direction, lower_left_corner, total_offset);
+                // and add it to our lower-left corner to get the final direction
+                vec3* direction = vec3_add(vec3_make(0, 0, 0), lower_left_corner, total_offset);
 
+                // create the ray with our camera position (origin) and calculated direction
                 ray3* r = ray3_make(origin, direction);
 
+                // get our color sample for our single ray from the world
                 vec3* color_sample = scene_color(r, surfaces, 2);
 
-                // add sample color to color total
-                color = vec3_add(color, color, color_sample);
+                // add sample color to color total for this pixel
+                pixel_color = vec3_add(pixel_color, pixel_color, color_sample);
 
                 // clean up
                 free(horizontal_offset);
                 free(vertical_offset);
                 free(total_offset);
+
                 free(direction);
-                free(r);
+                free(r); // direction is freed explicitly above; origin is used later so don't free
+
                 free(color_sample);
             }
             
-            // take average of each sample color
-            color = vec3_scale(color, color, 1.0 / num_samples);
+            // pixel_color contains the total of all samples; get the average of each sample
+            pixel_color = vec3_scale(pixel_color, pixel_color, 1.0 / num_samples);
 
             // write pixel color to file
-            printf("%d %d %d\n", (int) (color->x * 255), (int) (color->y * 255), (int) (color->z * 255));
+            printf("%d %d %d\n", (int) (pixel_color->x * 255), (int) (pixel_color->y * 255), (int) (pixel_color->z * 255));
 
             // clean up
-            free(color);
+            free(pixel_color);
         }
     }
+
+    // clean up
+    free(lower_left_corner);
+    free(horizontal);
+    free(vertical);
+    free(origin);
+
+    free(sphere->sphere_origin);
+    free(sphere);
+
+    free(sphere_big->sphere_origin);
+    free(sphere_big);
 }
 
      
