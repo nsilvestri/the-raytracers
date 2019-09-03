c2268d7a250b2807a27e8e5af376de3b367beb93
Nick Silvestri <nickpsilvestri@gmail.com>
2019-09-02 20:55:11 -0400

    add Scene.java

--- java/src/raytracer/Scene.java
+++ java/src/raytracer/Scene.java
@@ -1,5 +1,17 @@
 package raytracer;
 
+import java.util.List;
+
 public class Scene {
 
+	private List<Surface> surfaces;
+	
+	public Scene(List<Surface> surfaces) {
+		this.surfaces = surfaces;
+	}
+	
+	public Vec3 color(Ray3 r) {
+		
+	}
+	
 }
